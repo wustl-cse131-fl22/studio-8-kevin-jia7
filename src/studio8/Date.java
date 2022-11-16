ed9f867844a8de54886a54069241f51b5e965d84
贾顺文 <@ShunwenJia.dhcp.wustl.edu>
2022-11-16 17:16:03 -0600

    studio 8

--- src/studio8/Date.java
+++ src/studio8/Date.java
@@ -1,10 +1,78 @@
 package studio8;
 
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Objects;
+
 public class Date {
+	
+	private int year;
+	private int month;
+	private int day;
+	private boolean holiday;
 
+	public Date(int year, int month, int day, boolean holiday) {
+		this.year = year;
+		this.month = month;
+		this.day = day;
+		this.holiday = holiday;
+	}
 
-    public static void main(String[] args) {
+    @Override
+	public int hashCode() {
+		return Objects.hash(day, holiday, month, year);
+	}
 
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Date other = (Date) obj;
+		return day == other.day && holiday == other.holiday && month == other.month && year == other.year;
+	}
+
+	public static void main(String[] args) {		
+    	Date dateOne = new Date(2023, 01, 31, true);
+    	Date dateTwo = new Date(2023, 11, 31, true);
+    	
+    	System.out.println(dateOne);
+    	System.out.println(dateTwo);
+    	
+    	if (dateOne.hashCode() == dateTwo.hashCode()) {
+    		System.out.println("=");
+    	}
+    	else {
+    		System.out.println("!");
+    	}
+    	
+    	List<Date> list = new LinkedList<>();
+    	list.add(dateOne);
+    	list.add(dateTwo);
+    	list.add(dateTwo);
+    	System.out.println(list);
+    	
+    	HashSet<Date> set = new HashSet<Date>();
+    	set.add(dateOne);
+    	set.add(dateTwo);
+    	set.add(dateOne);
+    	System.out.println(set);
     }
 
+    public String toString() {
+    	
+    	String print = "";
+    	if (holiday) {
+    		print = year + "/" + month + "/" + day + ", today is a holiday!";
+    	}
+    	else {
+    		print = year + "/" + month + "/" + day + ", today is not a holiday.";
+    	}
+    	
+    	return print;
+    }
 }
--- src/studio8/Time.java
+++ src/studio8/Time.java
@@ -1,11 +1,74 @@
 package studio8;
 
+import java.util.Objects;
+import java.util.Scanner;
 
 public class Time {
+	
+	private int hour;
+	private int minute;
+	private boolean format;
 
-	public static void main(String[] args) {
- 
+	/**
+	 * 
+	 * @param hour: hour of the day
+	 * @param minute: minute of the hour
+	 * @param format: if true, use 12 hour format. if false, use 24 hour format.
+	 */
+	public Time(int hour, int minute, boolean format) {
+		this.hour = hour;
+		this.minute = minute;
+		this.format = format;
+	}
+
+    public static void main(String[] args) {
+    	Scanner in = new Scanner(System.in);
+    	Time timeOne = new Time(23, 59, false);
+    	Time timeTwo = new Time(23, 59, true);
     	
+    	System.out.println(timeOne);
+    	System.out.println(timeTwo);
+    	
+    	if (timeOne.hashCode() == timeTwo.hashCode()) {
+    		System.out.println("=");
+    	}
+    	else {
+    		System.out.println("!");
+    	}
     }
 
+    public String toString() {
+    	
+    	String print = "";
+    	if (format) {
+    		if (hour <= 12) {
+    			print = "Time of the day: " + hour + "am : " + minute;
+    		}
+    		else {
+    			print = "Time of the day: " + (hour - 12) + "pm : " + minute;
+    		}
+    	}
+    	else {
+    		print = "Time of the day: " + hour + " : " + minute;
+    	}
+    	return print;
+    }
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(hour, minute);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Time other = (Time) obj;
+		return hour == other.hour && minute == other.minute;
+	}
+    
 }
